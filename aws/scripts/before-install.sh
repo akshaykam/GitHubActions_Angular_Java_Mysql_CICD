cc01ef544a849e066dd3b32af10ea2a4a28f053f
Akshaykam_Official <72594492+akshaykam@users.noreply.github.com>
2023-10-17 14:18:26 +0530

    Update before-install.sh

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -8,6 +8,9 @@ fi
 
 mkdir -vp /usr/local/codedeployresources
 cd /usr/local/codedeployresources
+
 docker pull akshaykamthe651/github_actions_angular_java_mysql:${{ github.run_number }}  
 docker pull akshaykamthe651/github_actions_angularapp:${{ github.run_number }}
 docker images ls
+build_number= ${{ github.run_number }}
+export build_number
