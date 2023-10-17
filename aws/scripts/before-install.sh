d927f01ec21e32a064505454b17561cebd67945e
Akshaykam_Official <72594492+akshaykam@users.noreply.github.com>
2023-10-17 15:18:27 +0530

    Update before-install.sh
    docker tag updated

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -9,8 +9,8 @@ fi
 mkdir -vp /usr/local/codedeployresources
 cd /usr/local/codedeployresources
 
-docker pull akshaykamthe651/github_actions_angular_java_mysql:${{ github.run_number }}  
-docker pull akshaykamthe651/github_actions_angularapp:${{ github.run_number }}
+docker pull akshaykamthe651/github_actions_angular_java_mysql:${RUN_NUMBER} 
+docker pull akshaykamthe651/github_actions_angularapp:${RUN_NUMBER}
 docker images ls
-build_number= ${{ github.run_number }}
+build_number= ${RUN_NUMBER}
 export build_number
