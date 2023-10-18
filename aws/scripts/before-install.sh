5e8c7fc6d5eb7925032b6cbb6f3310922d1312ae
Akshaykam_Official <72594492+akshaykam@users.noreply.github.com>
2023-10-18 17:37:41 +0530

    Update before-install.sh

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -8,3 +8,6 @@ fi
 
 mkdir -vp /home/ec2-user/codeDeployment
 cd /home/ec2-user/codeDeployment
+
+# Ensure the ownership permissions are correct.
+chown -R ec2-user:ec2-user /home/ec2-user/codeDeployment
