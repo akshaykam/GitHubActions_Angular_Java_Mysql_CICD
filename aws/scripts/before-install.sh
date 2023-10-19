63495da80114346a45f825e5e76dd26d556427ca
Akshaykam_Official <72594492+akshaykam@users.noreply.github.com>
2023-10-19 07:35:03 +0530

    Update before-install.sh

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -2,12 +2,16 @@
 set -xe
 
 # Delete the old  directory as needed.
-if [ -d /home/ec2-user/codeDeployment ]; then
-    rm -rf /home/ec2-user/codeDeployment
+if [ -d /home/ec2-user/codeDeployment/GitHubActions_Angular_Java_Mysql_CICD ]; then
+    rm -rf /home/ec2-user/codeDeployment/GitHubActions_Angular_Java_Mysql_CICD
 fi
 
 mkdir -vp /home/ec2-user/codeDeployment
 cd /home/ec2-user/codeDeployment
+
+#manually crete ssh key on Ec2 and add it Github account
+git clone git@github.com:akshaykam/GitHubActions_Angular_Java_Mysql_CICD.git
+cd GitHubActions_Angular_Java_Mysql_CICD/
 
 # Ensure the ownership permissions are correct.
 chown -R ec2-user:ec2-user /home/ec2-user/codeDeployment
