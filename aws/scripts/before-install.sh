483932e3316c6900e8f29c377d03536ddea7520b
Akshaykam_Official <72594492+akshaykam@users.noreply.github.com>
2023-10-23 13:48:40 +0530

    Update before-install.sh

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -8,6 +8,7 @@ fi
 
 #ssh-keyscan github.com >> ~/.ssh/known_hosts
 mkdir -vp /home/ec2-user/codeDeployment
+chown -R ec2-user:ec2-user /home/ec2-user/codeDeployment
 cd /home/ec2-user/codeDeployment
 
 #manually crete ssh key on Ec2 and add it Github account
@@ -15,8 +16,11 @@ cd /home/ec2-user/codeDeployment
 ssh -vT git@github.com
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 ssh-keygen -t rsa -C "akshaykam@cybage.com"
+git -version
+echo "git clone stage"
 # git clone git@github.com:akshaykam/GitHubActions_Angular_Java_Mysql_CICD.git
 git clone https://github.com/akshaykam/GitHubActions_Angular_Java_Mysql_CICD.git
+echo "git clone is done"
 cd GitHubActions_Angular_Java_Mysql_CICD/
 
 # Ensure the ownership permissions are correct.
