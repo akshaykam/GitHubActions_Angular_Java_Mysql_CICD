321c8718dc7be6a60aef9affd97e7a1400445471
Akshaykam_Official <72594492+akshaykam@users.noreply.github.com>
2023-10-25 11:48:15 +0530

    Update before-install.sh
    clone test

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -10,7 +10,7 @@ fi
 mkdir -vp /home/ec2-user/codeDeployment
 chown -R ec2-user:ec2-user /home/ec2-user/codeDeployment
 cd /home/ec2-user/codeDeployment
-
+echo "folder created cloning repo"
 #manually crete ssh key on Ec2 and add it Github account
 #git clone https://akshaykam:${PRIVATE_REPO_PASS}@github.com/https://github.com/akshaykam/GitHubActions_Angular_Java_Mysql_CICD.git
 ssh -vT git@github.com
@@ -18,8 +18,8 @@ ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 ssh-keygen -t rsa -C "akshaykam@cybage.com"
 git -version
 echo "git clone stage"
-# git clone git@github.com:akshaykam/GitHubActions_Angular_Java_Mysql_CICD.git
-git clone https://github.com/akshaykam/GitHubActions_Angular_Java_Mysql_CICD.git
+git clone git@github.com:akshaykam/GitHubActions_Angular_Java_Mysql_CICD.git
+#git clone https://github.com/akshaykam/GitHubActions_Angular_Java_Mysql_CICD.git
 echo "git clone is done"
 cd GitHubActions_Angular_Java_Mysql_CICD/
 
